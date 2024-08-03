1503e51e338fcd290c202aafdabebfe17088925a
ishanrana363 <ishanrana363@gmail.com>
2024-08-04 01:04:11 +0600

    onclick

--- src/component/post-details/Postdetails.jsx
+++ src/component/post-details/Postdetails.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const Postdetails = () => {
     const postdetails = useLoaderData();
-    console.log(postdetails);
-    
+    const navigate = useNavigate()
+    const backData = ()=>{
+        navigate(-1);
+    };
+
     const {userId,id,title,body} = postdetails;
     return (
         <div>
@@ -12,6 +15,7 @@ const Postdetails = () => {
             <p> User id {id} </p>
             <h2> Title is : {title} </h2>
             <p> Postbody : {body} </p>
+            <button onClick={backData} className='bg-[#33eb33] py-2 px-10 my-6 rounded-2xl font-semibold text-[17px] hover:bg-[#a975d2] ' >Back</button>
         </div>
     );
 };
--- src/component/postdata/Postsdata.jsx
+++ src/component/postdata/Postsdata.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Postsdata = ({post}) => {
     const {userId,id,title} = post;
+    const navigate = useNavigate();
+    const onHandelChange =() =>{
+        navigate(`/post-details/${id}`)
+    }
     return (
         <div className=' 
         border-2 text-[25px] text-blue-600 font-medium  border-gray-950 h-auto w-auto rounded-lg shadow-lg
@@ -11,6 +15,7 @@ const Postsdata = ({post}) => {
             <h1> User Id is : { id } </h1>
             <p> Title is {title} </p>
             <Link className='text-black' to={`/post-details/${id}`} >Post Details </Link>
+            <button onClick={onHandelChange} className=' w-[150px] text-[17px] text-slate-700 font-bold bg-[#61e734] p-2 rounded-md shadow-lg ' >Post details</button>
         </div>
     );
 };
